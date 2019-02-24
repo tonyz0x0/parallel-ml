33c0d0643df9d40ee14a61e0aa4b4d490e241cbe
BigTony <kejiezhang666@gmail.com>
2019-02-23 23:34:58 -0500

    feat: add ParallelRegression script

--- ParallelRegression.py
+++ ParallelRegression.py
@@ -7,59 +7,56 @@ from time import time
 from pyspark import SparkContext
 
 
-def readData(input_file,spark_context):
-    """  Read data from an input file and return rdd containing pairs of the form:
-	                 (x,y)
-	 where x is a numpy array and y is a real value. The input file should be a 
-	 'comma separated values' (csv) file: each line of the file should contain x
-         followed by y. For example, line:
+def readData(input_file, spark_context):
+    """
+    Read data from an input file and return rdd containing pairs of the form: (x,y)
+    where x is a numpy array and y is a real value. The input file should be a
+    'comma separated values' (csv) file: each line of the file should contain x
+    followed by y. For example, line:
 
          1.0,2.1,3.1,4.5
 
-         should be converted to tuple:
-	
+    should be converted to tuple:
+
          (array(1.0,2.1,3.1),4.5)
-	 
 
-    """ 
-    return spark_context.textFile(input_file)\
-		.map(lambda line:line.split(','))\
-		.map(lambda words:(words[:-1],words[-1]))\
-		.map(lambda (features,target): (np.array([ float(x) for x in features]),float(target)))
-
+    """
+    return spark_context.textFile(input_file) \
+        .map(lambda line: line.split(',')) \
+        .map(lambda words: (words[:-1], words[-1])) \
+        .map(lambda (features, target): (np.array([float(x) for x in features]), float(target)))
 
 
 def readBeta(input):
     """ Read a vector β from CSV file input
     """
-    with open(input,'r') as fh:
-        str_list = fh.read()\
-                   .strip()\
-		   .split(',')
-        return np.array( [float(val) for val in str_list] )           
+    with open(input, 'r') as fh:
+        str_list = fh.read() \
+            .strip() \
+            .split(',')
+        return np.array([float(val) for val in str_list])
 
-def writeBeta(output,beta):
+
+def writeBeta(output, beta):
     """ Write a vector β to a CSV file ouptut
     """
-    with open(output,'w') as fh:
-	fh.write(','.join(map(str, beta.tolist()))+'\n')
-    
+    with open(output, 'w') as fh:
+        fh.write(','.join(map(str, beta.tolist())) + '\n')
 
 
+def estimateGrad(fun, x, delta):
+    """ Given a real-valued function fun, estimate its gradient numerically.
+    """
+    d = len(x)
+    grad = np.zeros(d)
+    for i in range(d):
+        e = np.zeros(d)
+        e[i] = 1.0
+        grad[i] = (fun(x + delta * e) - fun(x)) / delta
+    return grad
 
-def estimateGrad(fun,x,delta):
-     """ Given a real-valued function fun, estimate its gradient numerically.
-     """
-     d = len(x)
-     grad = np.zeros(d)
-     for i in range(d):
-         e = np.zeros(d)
-         e[i] = 1.0
-         grad[i] = (fun(x+delta*e) - fun(x))/delta
-     return grad
 
- 
-def lineSearch(fun,x,grad,a=0.2,b=0.6):
+def lineSearch(fun, x, grad, a=0.2, b=0.6):
     """ Given function fun, a current argument x, and gradient grad, 
 	perform backtracking line search to find the next point to move to.
 	(see Boyd and Vandenberghe, page 464).
@@ -72,39 +69,32 @@ def lineSearch(fun,x,grad,a=0.2,b=0.6):
 	The return value is the resulting value of t.
     """
     t = 1.0
-    while fun(x-t*grad) > fun(x)- a * t *np.dot(grad,grad):
-	t = b * t
-    return t 
-    
+    while fun(x - t * grad) > fun(x) - a * t * np.dot(grad, grad):
+        t = b * t
+    return t
 
 
-def predict(x,beta):
+def predict(x, beta):
     """ Given vector x containing features and parameter vector β, 
 	return the predicted value: 
 
 	                y = <x,β>   
 
     """
-    pass
+    return np.dot(x, beta)
 
 
-
-
-def f(x,y,beta):
+def f(x, y, beta):
     """ Given vector x containing features, true label y, 
 	and parameter vector β, return the square error:
 
 	         f(β;x,y) =  (y - <x,β>)^2	
 
     """
-    pass
+    return (y - predict(x, beta)) ** 2
 
 
-
-
-
-
-def localGradient(x,y,beta):
+def localGradient(x, y, beta):
     """ Given vector x containing features, true label y, 
 	and parameter vector β, return the gradient ∇f of f:
 
@@ -114,11 +104,10 @@ def localGradient(x,y,beta):
 
         The return value is  ∇f.
     """
-    pass
+    return -2 * (y - predict(x, beta)) * x
 
- 
 
-def F(data,beta,lam = 0):
+def F(data, beta, lam=0):
     """  Compute the regularized mean square error:
 
              F(β) = 1/n Σ_{(x,y) in data}    f(β;x,y)  + λ ||β ||_2^2   
@@ -135,10 +124,14 @@ def F(data,beta,lam = 0):
 	 The return value is F(β).
 
     """
-    pass
+    n = data.count()
+    MSE = 1. / n * data.map(lambda (x, y): f(x, y, beta)).reduce(add)
+    Reg = lam * np.dot(beta, beta)
+
+    return MSE + Reg
 
 
-def gradient(data,beta,lam = 0):
+def gradient(data, beta, lam=0):
     """ Compute the gradient  ∇F of the regularized mean square error 
                 F(β) = 1/n Σ_{(x,y) in data} f(β;x,y) + λ ||β ||_2^2   
                      = 1/n Σ_{(x,y) in data} (y- <x,β>)^2 + λ ||β ||_2^2   
@@ -154,9 +147,12 @@ def gradient(data,beta,lam = 0):
         The return value is an array containing ∇F.
 
     """
-    pass
+    n = data.count()
+    grad = 1. / n * data.map(lambda (x, y): localGradient(x, y, beta)).reduce(add)
+    return grad + 2 * lam * beta
 
-def test(data,beta):
+
+def test(data, beta):
     """ Compute the mean square error  
 
 		 MSE(β) =  1/n Σ_{(x,y) in data} (y- <x,β>)^2
@@ -170,12 +166,10 @@ def test(data,beta):
 	The return value is MSE(β).  
        
     """
-    pass
+    return F(data, beta)
 
 
-
-
-def train(data,beta_0, lam,max_iter,eps):
+def train(data, beta_0, lam, max_iter, eps):
     """ Perform gradient descent:
 
 
@@ -186,7 +180,7 @@ def train(data,beta_0, lam,max_iter,eps):
 	where
              - data: an rdd containing pairs of the form (x,y)
              - beta_0: the starting vector β
-	     - lam:  is the regularization parameter λ
+	         - lam:  is the regularization parameter λ
              - max_iter: maximum number of iterations of gradient descent
              - eps: upper bound on the l2 norm of the gradient
              - a,b: parameters used in backtracking line search
@@ -207,21 +201,41 @@ def train(data,beta_0, lam,max_iter,eps):
 	The function returns:
 	     -beta: the trained β, 
 	     -gradNorm: the norm of the gradient at the trained β, and
-             -k: the number of iterations performed
-    """ 	
-    pass
+         -k: the number of iterations performed
+    """
+    k = 0
+    beta = beta_0
+    gradNorm = 2 * eps
+    start = time()
 
-   
+    while k < max_iter and eps < gradNorm:
+        grad = gradient(data, beta, lam)
+        fun = lambda x: F(data, x, lam)
+        gamma = lineSearch(fun, beta, grad)
+        beta = beta - gamma * grad
+        gradNorm = np.linalg.norm(grad)
+        reg_mse = F(data, beta, lam)
+        end = time() - start;
+        k += 1
+        print "k = {0}\t time = {1}\t F(βk) = {2}\t ||∇F(β_k)||2 = {3}".format(k, end, reg_mse, gradNorm)
+
+    return beta, gradNorm, k
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description = 'Parallel Ridge Regression.',formatter_class=argparse.ArgumentDefaultsHelpFormatter)    
-    parser.add_argument('--traindata',default=None, help='Input file containing (x,y) pairs, used to train a linear model')
-    parser.add_argument('--testdata',default=None, help='Input file containing (x,y) pairs, used to test a linear model')
-    parser.add_argument('--beta', default='beta', help='File where beta is stored (when training) and read from (when testing)')
-    parser.add_argument('--lam', type=float,default=0.0, help='Regularization parameter λ')
-    parser.add_argument('--max_iter', type=int,default=100, help='Maximum number of iterations')
-    parser.add_argument('--eps', type=float, default=0.01, help='ε-tolerance. If the l2_norm gradient is smaller than ε, gradient descent terminates.') 
-    parser.add_argument('--N',type=int,default=2,help='Level of parallelism')
-
+    parser = argparse.ArgumentParser(description='Parallel Ridge Regression.',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--traindata', default=None,
+                        help='Input file containing (x,y) pairs, used to train a linear model')
+    parser.add_argument('--testdata', default=None,
+                        help='Input file containing (x,y) pairs, used to test a linear model')
+    parser.add_argument('--beta', default='beta',
+                        help='File where beta is stored (when training) and read from (when testing)')
+    parser.add_argument('--lam', type=float, default=0.0, help='Regularization parameter λ')
+    parser.add_argument('--max_iter', type=int, default=100, help='Maximum number of iterations')
+    parser.add_argument('--eps', type=float, default=0.01,
+                        help='ε-tolerance. If the l2_norm gradient is smaller than ε, gradient descent terminates.')
+    parser.add_argument('--N', type=int, default=2, help='Level of parallelism')
 
     verbosity_group = parser.add_mutually_exclusive_group(required=False)
     verbosity_group.add_argument('--verbose', dest='verbose', action='store_true')
@@ -229,40 +243,39 @@ if __name__ == "__main__":
     parser.set_defaults(verbose=True)
 
     args = parser.parse_args()
-  
+
     sc = SparkContext(appName='Parallel Ridge Regression')
-    
-    if not args.verbose :
-        sc.setLogLevel("ERROR")	
-  
+
+    if not args.verbose:
+        sc.setLogLevel("ERROR")
+
     beta = None
 
+    print "LAM: {}".format(args.lam)
     if args.traindata is not None:
         # Train a linear model β from data with regularization parameter λ, and store it in beta
-        print 'Reading training data from',args.traindata
-        data = readData(args.traindata,sc)
+        print 'Reading training data from', args.traindata
+        data = readData(args.traindata, sc)
         data = data.repartition(args.N).cache()
-      
-        x,y = data.take(1)[0]
+
+        x, y = data.take(1)[0]
         beta0 = np.zeros(len(x))
 
-	print 'Training on data from',args.traindata,'with λ =',args.lam,', ε =',args.eps,', max iter = ',args.max_iter
-        beta, gradNorm, k = train(data,beta_0=beta0,lam=args.lam,max_iter=args.max_iter,eps=args.eps) 
-	print 'Algorithm ran for',k,'iterations. Converged:',gradNorm<args.eps
-	print 'Saving trained β in',args.beta
-        writeBeta(args.beta,beta)
-        
-     
+        print 'Training on data from', args.traindata, 'with λ =', args.lam, ', ε =', args.eps, ', max iter = ', args.max_iter
+        beta, gradNorm, k = train(data, beta_0=beta0, lam=args.lam, max_iter=args.max_iter, eps=args.eps)
+        print 'Algorithm ran for', k, 'iterations. Converged:', gradNorm < args.eps
+        print 'Saving trained β in', args.beta
+        writeBeta(args.beta, beta)
+
     if args.testdata is not None:
         # Read beta from args.beta, and evaluate its MSE over data
-        print 'Reading test data from',args.testdata
-        data = readData(args.testdata,sc)
+        print 'Reading test data from', args.testdata
+        data = readData(args.testdata, sc)
         data = data.repartition(args.N).cache()
-      
-        print 'Reading beta from',args.beta
-	beta = readBeta(args.beta)
 
-	print 'Computing MSE on data',args.testdata
-        MSE = test(data,beta)
-	print 'MSE is:', MSE 
-	
+        print 'Reading beta from', args.beta
+        beta = readBeta(args.beta)
+
+        print 'Computing MSE on data', args.testdata
+        MSE = test(data, beta)
+        print 'MSE is:', MSE
